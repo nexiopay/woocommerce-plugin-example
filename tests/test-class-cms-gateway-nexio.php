d931d6942f89aa2800b17f57ad8f77e7b172143e
Sam Lu <samlu@cmsonline.com>
2019-05-10 13:53:59 +0800

    remove $GLOBAL test condition
    remove $GLOBAL test condition

--- tests/test-class-cms-gateway-nexio.php
+++ tests/test-class-cms-gateway-nexio.php
@@ -415,7 +415,6 @@ class TestClassCMSGatewayNexio extends WC_Unit_Test_Case{
         $mockedObject->api_url = 'https://api.nexiopay.com/';
         $onetimetoken = $mockedObject->get_iframe_src('123456');
 
-        $mockedObject->$GLOBALS['is_IE'] = '';
 
         $return = $mockedObject->generate_nexio_form($order_id);
         $return2 = '<form id="cms_payment_form" action="'.esc_url( $onetimetoken ).'" method="post">
@@ -471,7 +470,6 @@ class TestClassCMSGatewayNexio extends WC_Unit_Test_Case{
         $mockedObject->api_url = 'https://api.nexiopaysandbox.com/';
         $onetimetoken = $mockedObject->get_iframe_src('123456');
         
-        $mockedObject->$GLOBALS['is_IE'] = '';
         $return = $mockedObject->generate_nexio_form($order_id);
         $return2 = '<form id="cms_payment_form" action="'.esc_url( $onetimetoken ).'" method="post">
 		<iframe type="iframe" class="cms_iframe" id="iframe1" src="'.$onetimetoken.'"></iframe><div id="loader"></div>
